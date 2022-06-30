56534f696fc87e7a21925ea18f8062bc7f3864d1
dydgks789@naver.com <dydgks789@naver.com>
2022-06-30 22:03:14 +0900

    Update

--- BOJ/Deque/5430.py
+++ BOJ/Deque/5430.py
@@ -1,43 +1,37 @@
 # AC
 from collections import deque
 
-t = int(input())
-deq = deque()
-for i in range(t):
+for i in range(int(input())):
+    command = input()
+    size = int(input())
+    
+    nums = input()[1:-1].split(',')
+    q = deque()
+    for j in range(size):
+        q.append(nums[j])
+        
     result = 0
-    repR = 0
-    func = str(input())
-    length = int(input())
-    num = input()
-    nums = num[1:-1].split(',')
-    for j in range(length):
-        deq.append(int(nums[j]))
-    for k in func:
+    r_count = 0
+    for k in command:
         if k == 'R':
-            repR += 1
+            r_count += 1
         else:
-            if repR % 2 == 0:
-                if deq:
-                    deq.popleft()
+            if r_count % 2 == 0:
+                if q:
+                    q.popleft()
                 else:
                     result = -1
                     break
             else:
-                if deq:
-                    deq.pop()
+                if q:
+                    q.pop()
                 else:
                     result = -1
                     break
-    if repR % 2 != 0:
-        deq.reverse()
+
+    if r_count % 2 != 0:
+        q.reverse()
     if result == 0:
-        print('[', end='')
-        for u in range(len(deq)):
-            if u != len(deq) - 1:
-                print(deq[u], end=',')
-            else:
-                print(f'{deq[u]}', end='')
-        print(']')
+        print('['+','.join(q)+']')
     else:
         print('error')
-    deq.clear()
---
+++ BOJ/Sorting/2018.py
@@ -0,0 +1,24 @@
+# 통계학
+import sys
+from collections import Counter
+n = int(input())
+nums = []
+sum = 0
+for i in range(n):
+    num = int(sys.stdin.readline())
+    nums.append(num)
+    sum += num    
+nums.sort()
+print(round(sum/n))
+print(nums[n//2])
+
+count = Counter(nums).most_common()
+if len(count) > 1:
+    if count[1][1] == count[0][1]:
+        print(count[1][0])
+    else:
+        print(count[0][0])
+else:
+    print(count[0][0])
+print(nums[-1] - nums[0])
+
