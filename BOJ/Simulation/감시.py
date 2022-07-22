ec66e4e88def2ca5735fe10e0fcece4d38d37203
dydgks789@naver.com <dydgks789@naver.com>
2022-07-22 18:08:37 +0900

    'Update'

---
+++ BOJ/Simulation/감시.py
@@ -0,0 +1,36 @@
+# import copy
+
+# n, m = map(int, input().split())
+# graph = [list(map(int, input().split())) for _ in range(n)]
+# monitored = copy.deepcopy(graph)
+# cctv = []
+# result = []
+# for i in range(n):
+#     for j in range(m):
+#         if graph == 0 or graph == 6:
+#             continue
+#         cctv.append(graph[i][j])
+
+# def turn()
+
+# c1 = []
+
+# def monitor(k):
+#     if k == 1:
+        
+#     elif k == 2:
+#     elif k == 3:
+#     elif k == 4:
+#     elif k == 5:
+
+
+
+# for i in range(len(cctv)):
+#     monitor(cctv[i])
+
+
+
+
+
+
+
