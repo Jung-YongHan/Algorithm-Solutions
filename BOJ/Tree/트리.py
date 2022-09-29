44e6798ea4e9c662bb117069ceed8304f988b9c5
dydgks789@naver.com <dydgks789@naver.com>
2022-09-29 20:47:55 +0900

    Update

--- BOJ/Tree/트리.py
+++ BOJ/Tree/트리.py
@@ -1,46 +1,31 @@
 # 1068
-# https://77dptjd.tistory.com/13 참고하여 다시 풀기 22.09.29
+# dfs를 이용하면 가지치기는 쉬운 문제
+# 테스트케이스가 생각보다 다양하여 예외처리 하기가 까다로웠다.
 def remove_node(root):
-    if tree[root] != [None]:
-        values = tree[root]
-        for i in values:
+    tree[root] = -2
+    for i in range(n):
+        if tree[i] == root:
+            tree[i] = -2
             remove_node(i)
-            del tree[i]
 
 n = int(input())
-tree = {}
-tmp = [[] for _ in range(n)]
-for i, root in enumerate(list(map(int, input().split()))):
-    if root != -1:
-        tmp[root].append(i)
-
-for i in range(n):
-    if tmp[i]:
-        tree[i] = tmp[i]
-    else:
-        tree[i] = [None]
-
+tree = list(map(int, input().split()))
 remove = int(input())
-if remove == 0:
-    del tree
-    print(0)
-else:
-    remove_node(remove) # 가지치기
-    del tree[remove] # 해당 노드도 제거
 
-    cnt = 0
-    for key in tree.keys():
-        if tree[key] != [None]:
-            leaf = False
-            for value in tree[key]:
-                if value in tree:
-                    leaf = False
-                    break
-                else:
-                    leaf = True
-            if leaf:
-                tree[key] = [None]
-    for key in tree.keys():
-        if tree[key] == [None]:
-            cnt += 1
-    print(cnt)
+remove_node(remove)
+count = 0
+for i in range(n):
+    if tree[i] != -2:
+        is_avl = True
+        for j in tree: # i노드가 -2가 아니면서 
+            if i == j: # 자신을 부모노드로 갖지 않는다면
+                is_avl = False
+                break
+        if is_avl:
+            count+=1
+print(count)
+
+# 20line은 -2가 아니라면 루트노드이거나 리프노드 또는 
+# 자식노드를 가질 수 있는 부모노드이다.
+# 만일 루트노드거나 자식노드가 있는 부모노드라면 리프노드가 아니므로 개수 추가 X
+# 자신을 부모노드로 갖지않는 -> 리프노드라면 개수 추가해주었다.
