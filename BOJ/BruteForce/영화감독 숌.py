773dbb17411b2e4f438d51fa0376f9bbae2cd5bc
dydgks789@naver.com <dydgks789@naver.com>
2022-12-26 21:57:05 +0900

    Update

---
+++ BOJ/BruteForce/영화감독 숌.py
@@ -0,0 +1,8 @@
+# 1436
+
+n = int(input())
+arr = []
+for i in range(666, int(3e6)):
+    if '666' in str(i):
+        arr.append(str(i))
+print(arr[n-1])
--- BOJ/solving.py
+++ BOJ/solving.py
@@ -1,9 +1,8 @@
-import sys
-
-n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, sys.stdin.readline().split())))
-arr.sort(key=lambda x:(x[0], x[1]))
-for i in range(n):
-    print(*arr[i])
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+    arr = [x for x in range(1, n+1)]
+    for i in range(k):
+        for j in range(1, n):
+            arr[j] += arr[j-1]
+    print(arr[-1])
