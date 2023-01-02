fb735c80e84102bfaa51258b7264904205fa6c73
dydgks789@naver.com <dydgks789@naver.com>
2023-01-02 20:59:10 +0900

    Update

--- BOJ/DynamicProgramming/피보나치 함수.py
+++ BOJ/DynamicProgramming/피보나치 함수.py
@@ -1,20 +1,12 @@
-# 1003
 import sys
-input = sys.stdin.readline
-result0 = [0] * 42
-result1 = [0] * 42
-result0[0] = result1[1] = result0[2] = result0[3] = result1[2] = 1
-result1[3] = 2
-for i in range(int(input())):
-    n = int(input())
-    def fibo(n):
 
-        if n <= 3:
-            print(result0[n], result1[n])
-            return
-        for i in range(4, n+1):
-            result0[i] = result0[i-1] + result0[i-2]
-            result1[i] = result1[i-1] + result1[i-2]
-        print(result0[n], result1[n])
-        return
-    fibo(n)
+dp = [[0, 0] for _ in range(41)]
+dp[0][0] = 1
+dp[1][1] = 1
+
+for i in range(2, 41):
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]
+    
+for _ in range(int(input())):
+    print(*dp[int(sys.stdin.readline())])
--- BOJ/solving.py
+++ BOJ/solving.py
@@ -1,15 +1,10 @@
-import sys
+dp = [[0, 0] for _ in range(41)]
+dp[0][0] = 1
+dp[1][1] = 1
 
-n, m = map(int, input().split())
-arr1 = set()
-arr2 = list()
-for i in range(n+m):
-    s = sys.stdin.readline().rstrip()
-    if s in arr1:
-        arr2.append(s)
-    else:
-        arr1.add(s)
-arr2.sort()
-print(len(arr2))
-for i in arr2:
-    print(i)
+for i in range(2, 41):
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]
+
+for _ in range(int(input())):
+    print(*dp[int(input())])
