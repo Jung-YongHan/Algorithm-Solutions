a74edf780f014ad3da25cc2aafdfe7d91afb636f
dydgks789@naver.com <dydgks789@naver.com>
2023-01-04 11:10:33 +0900

    Update

--- BOJ/Greedy/잃어버린 괄호.py
+++ BOJ/Greedy/잃어버린 괄호.py
@@ -1,12 +1,9 @@
 # 1541
 s = input().split('-')
-sum = 0
-for i in s[0].split('+'):
-    sum += int(i)
-for i in s[1:]:
-    for j in i.split('+'):
-        sum -= int(j)
-print(sum)
+result = []
+for i in s:
+    result.append(sum(map(int, i.split('+'))))
+print(result[0]-sum(result[1:]))
             
 
     
--- BOJ/solving.py
+++ BOJ/solving.py
@@ -1,10 +1,5 @@
-dp = [[0, 0] for _ in range(41)]
-dp[0][0] = 1
-dp[1][1] = 1
-
-for i in range(2, 41):
-    dp[i][0] = dp[i-1][0] + dp[i-2][0]
-    dp[i][1] = dp[i-1][1] + dp[i-2][1]
-
-for _ in range(int(input())):
-    print(*dp[int(input())])
+s = input().split('-')
+result = []
+for i in s:
+    result.append(eval(i))
+print(result[0]-sum(result[1:]))
