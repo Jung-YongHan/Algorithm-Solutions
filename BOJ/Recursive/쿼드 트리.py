34a8d9c5c73c24cdc67879ec22a26e49c6ecb634
dydgks789@naver.com <dydgks789@naver.com>
2023-01-10 23:35:18 +0900

    Update

--- BOJ/Recursive/쿼드 트리.py
+++ BOJ/Recursive/쿼드 트리.py
@@ -1,24 +1,25 @@
 # 1992
 
-def isValid(x, y, n):
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input())))
+
+def solve(x, y, n):
+    if check(x, y, n):
+        print(arr[x][y], end="")
+    else:
+        print("(", end="")
+        for i in range(2):
+            for j in range(2):
+                solve(x+i*n//2, y+j*n//2, n//2)
+        print(")", end="")
+
+def check(x, y, n):
     for i in range(x, x+n):
         for j in range(y, y+n):
-            if graph[x][y] != graph[i][j]:
+            if arr[x][y] != arr[i][j]:
                 return False
     return True
 
-def solve(x, y, n):
-    if isValid(x, y, n):
-        print(graph[x][y], end='')
-        return 
-    
-    half = n // 2
-    print('(', end='')
-    for i in range(2):
-        for j in range(2):
-            solve(x+i*half, y+j*half, half)
-    print(')', end='')
-
-n = int(input())
-graph = [list(map(int, input())) for _ in range(n)]
 solve(0, 0, n)
--- BOJ/solving.py
+++ BOJ/solving.py
@@ -1,5 +1,23 @@
-s = input().split('-')
-result = []
-for i in s:
-    result.append(eval(i))
-print(result[0]-sum(result[1:]))
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input())))
+
+def solve(x, y, n): # 재귀 함수
+    if check(x, y, n):
+        print(arr[x][y], end="")
+    else:
+        print("(", end="")
+        for i in range(2):
+            for j in range(2):
+                solve(x+i*n//2, y+j*n//2, n//2)
+        print(")", end="")
+
+def check(x, y, n): # 같은 숫자인지 확인하는 함수
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if arr[x][y] != arr[i][j]:
+                return False
+    return True
+
+solve(0, 0, n)
