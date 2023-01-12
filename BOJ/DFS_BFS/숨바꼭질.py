87dd2900cd4b16d06f3ddb4d1ca029327ab6dcdc
dydgks789@naver.com <dydgks789@naver.com>
2023-01-12 21:53:40 +0900

    Update

--- BOJ/DFS_BFS/숨바꼭질.py
+++ BOJ/DFS_BFS/숨바꼭질.py
@@ -1,19 +1,15 @@
-# 1697
-# 1차원 배열에서의 BFS 최단거리 문제
 from collections import deque
 
 n, k = map(int, input().split())
-graph = [-1] * 100001
-graph[n] = 0
-
-def bfs():
-    q = deque([n])
-    while q:
-        x = q.popleft()
-        if x == k:
-            return graph[x]
-        for nx in [x-1, x+1, 2*x]:
-            if 0 <= nx <= 100000 and graph[nx] == -1:
-                graph[nx] = graph[x] + 1
-                q.append(nx)
-print(bfs())
+arr = [-1]*(100001)
+arr[n] = 0
+q = deque([n])
+while True:
+    x = q.popleft()
+    if x == k:
+        print(arr[k])
+        break
+    for i in [x-1, x+1, 2*x]:
+        if 0<=i<=100000 and arr[i]==-1:
+            q.append(i)
+            arr[i] = arr[x]+1
--- BOJ/solving.py
+++ BOJ/solving.py
@@ -1,34 +1,20 @@
 from collections import deque
-import sys
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+n, m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
 
-def bfs(u, v):
-    q = deque([(u, v)])
-    graph[u][v] = 0
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 1:
-                    q.append((nx, ny))
-                    graph[nx][ny] = 0
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-for _ in range(int(input())):
-    m, n, k = map(int, input().split())
-    graph = [[0]*m for _ in range(n)]
-    for _ in range(k):
-        u, v = map(int, sys.stdin.readline().split())
-        graph[v][u] = 1
-    
-    result = 0
-    for x in range(n):
-        for y in range(m):
-            if graph[x][y] == 1:
-                bfs(x, y)
-                result += 1
-
-    print(result)
+q = deque([(0, 0)])
+arr[0][0] = 0
+while q:
+    x, y = q.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<m:
+            if arr[nx][ny] == 1:
+                q.append((nx, ny))
+                arr[nx][ny] = arr[x][y] + 1
+print(arr[n-1][m-1]+1)
